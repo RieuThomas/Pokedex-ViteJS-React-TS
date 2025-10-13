622e89d256c4f3ea132f4e79783f7b372eed698e
Thomas Rieu <rieu.thomas@hotmail.com>
2025-10-13 15:49:12 +0200

    useState management for the button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
@@ -13,9 +14,23 @@ const pokemonList = [
 ];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("bulbasaur");
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemon} />
+      <button type="button" onClick={() => setPokemonName("bulbasaur")}>
+        bulbisaur
+      </button>
+      <button type="button" onClick={() => setPokemonName("mew")}>
+        mew
+      </button>
     </div>
   );
 }
