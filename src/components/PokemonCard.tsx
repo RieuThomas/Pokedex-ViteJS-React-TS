3cfdda95daa06df2bd093460f2d2fb2d88e9e5b7
Thomas Rieu <rieu.thomas@hotmail.com>
2025-10-13 14:58:05 +0200

    addition of variable in component

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,23 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
-  return (
+  const pokemon = pokemonList[1];
+  return pokemon.imgSrc ? (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      <img src={pokemon.imgSrc} alt={pokemon.name} />
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
+  ) : (
+    <p>???</p>
   );
 }
 
